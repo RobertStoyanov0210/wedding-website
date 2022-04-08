7e323c7ad4e9d8eeb12072fb77fc2e3171ce2737
Robert Stanislavov Stoyanov <rstoianov0210@gmail.com>
2022-04-09 01:13:39 +0300

    added footer info + changes in header

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,6 @@ window.addEventListener('DOMContentLoaded', event => {
         hour = minute * 60,
         day = hour * 24;
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
   let today = new Date(),
       dd = String(today.getDate()).padStart(2, "0"),
       mm = String(today.getMonth() + 1).padStart(2, "0"),
@@ -37,17 +35,9 @@ window.addEventListener('DOMContentLoaded', event => {
         const now = new Date().getTime(),
               distance = countDown - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
           document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
           document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
 
-        //do something later when date is reached
-        if (distance < 0) {
-          document.getElementById("headline").innerText = "Йей";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
-          clearInterval(x);
-        }
-        //seconds
       }, 0)
   }());
